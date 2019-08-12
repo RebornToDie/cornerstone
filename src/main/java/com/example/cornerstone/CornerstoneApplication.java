f1b9638bf31b3d47b675cf8e9914066f92454aba
xh <1481719687@qq.com>
2019-08-12 18:27:10 +0800

    page access

--- src/main/java/com/example/cornerstone/CornerstoneApplication.java
+++ src/main/java/com/example/cornerstone/CornerstoneApplication.java
@@ -1,12 +1,20 @@
 package com.example.cornerstone;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Properties;
 
 @SpringBootApplication
 public class CornerstoneApplication {
 
+    private static Logger log = LoggerFactory.getLogger(CornerstoneApplication.class);
+
     public static void main(String[] args) {
+        Properties properties = System.getProperties();
+        log.info("CornerstoneApplication workdir : {}",properties.get("user.dir"));
         SpringApplication.run(CornerstoneApplication.class, args);
     }
 
